ecdacb10f9e44bca13daabaac698d1ac92ea0a77
Nicolae001 <131882891+Nicolae001@users.noreply.github.com>
2025-03-30 16:33:48 +0200

    Delete ing-software/main/java/unibs/progetto/Fattore.java

--- ing-software/main/java/unibs/progetto/Fattore.java
+++
@@ -1,9 +0,0 @@
-package unibs.progetto;
-
-import java.io.Serializable;
-
-public class Fattore implements Serializable{
-    private static final  Double min=0.5;
-    private static final  Double max=2.0;
-    
-}
